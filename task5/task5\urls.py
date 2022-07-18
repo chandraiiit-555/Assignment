59cce4d02d96f30e3ab3161e129339c244b3ba5f
chandraiiit-555 <58101680+chandraiiit-555@users.noreply.github.com>
2022-07-18 11:02:59 +0530

    Delete task5\urls.py

--- task5/task5\urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-
-from task.views import task5
-urlpatterns = [
-    path('admin/', admin.site.urls), # by default admin.site.urls will create
-    path('task5', task5),
-
-]
